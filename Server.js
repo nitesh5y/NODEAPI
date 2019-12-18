ce2ef3b1b8739069bf0cfc5b291c3afa2b68b281
Nitesh.Singhal <Nitesh.Singhal@condecosoftware.com>
2019-12-18 15:34:39 +0530

    Added comments in server js and rename Conect file

--- Server.js
+++ Server.js
@@ -2,14 +2,21 @@ const express =require("express");
 const mongoose =require("mongoose");
 const bodyparser =require("body-parser");
 const app =  express();
-require("./mongo")
 
+//DB Connection
+require("./mongoDBConnection")
+
+
+//Importing Models
 const test =require("./model/Category")
 require("./model/Product")
 console.log(test)
+
+
+//Using bodyparser in express
 app.use(bodyparser.json());
 
-
+//Using Multiple CustomRouters in express
 app.use("/category",require("./routes/CategoryRoute"));
 app.use("/product",require("./routes/ProductRoute"));
 
